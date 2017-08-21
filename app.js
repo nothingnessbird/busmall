2fc0fb6382c02b615fd388acdb92337833f62f15
Kinley Ramson <kinleyramson@gmail.com>
2017-08-21 14:55:30 -0700

    made a randomizer function to generate a random number from 0-18, made an array for image tag ids, starting on how to display

--- app.js
+++ app.js
@@ -3,16 +3,29 @@
 var productFiles = ['bag.jpg','banana.jpg','bathroom.jpg','boots.jpg','breakfast.jpg','bubblegum.jpg','chair.jpg','cthulhu.jpg','dog-duck.jpg','dragon.jpg','pen.jpg','pet-sweep.jpg','scissors.jpg','shark.jpg','sweep.png','tauntaun.jpg','unicorn.jpg','usb.gif','water-can.jpg','wine-glass.jpg'];
 
 function Product(name,fileLink) {
-  name = this.name;
-  fileLink = this.fileLink;
+  this.name = name;
+  this.fileLink = fileLink;
+  this.shownCounter = 0;
+  this.clickCounter = 0;
 };
 
 var productArray = [];
 
 var productMaker = function () {
   for (var i = 0; i < productFiles.length; i++) {
-    fileNamer = productFiles[i].slice(0,-4);
-    fileLinker = productFiles[i];
+    var fileNamer = productFiles[i].slice(0,-4);
+    var fileLinker = productFiles[i];
     productArray.push(new Product(fileNamer,fileLinker));
   }
 };
+
+productMaker();
+
+function randomizer(){
+  return Math.floor(Math.random() * productFiles.length);
+};
+
+var imgArray = ['product1','product2','product3'];
+
+var product1 = document.getElementById('product1');
+product1.setAttribute('src','img/bag.jpg');
