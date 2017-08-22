57274f866f7258ac78fd30125d34c24684d34265
Kinley Ramson <kinleyramson@gmail.com>
2017-08-21 17:23:45 -0700

    made functions to display list and display images and display button

--- app.js
+++ app.js
@@ -26,23 +26,65 @@ function randomizer(){
 };
 
 var imgArray = ['product1','product2','product3'];
+var lastImageArray = [];
 
 var productShower = function () {
   var currentImageArray = [];
   for (var i = 0; i < imgArray.length; i++) {
     var currentProduct = randomizer();
-    if (!currentImageArray.includes(currentProduct)) {
+    if (!currentImageArray.includes(currentProduct) && !lastImageArray.includes(currentProduct)) {
       currentImageArray.push(currentProduct);
       var displayImage = document.getElementById(imgArray[i]);
       displayImage.setAttribute('src','img/' + productArray[currentProduct].fileLink);
+      productArray[currentProduct].shownCounter++;
     } else {
       i--;
     }
   }
+  lastImageArray = currentImageArray;
 };
 
-// productShower();
+var clicks = 0;
 
-var products = document.getElementById('products');
+var startButton = function () {
+  button.style.visibility = 'hidden';
+  productShower();
+};
 
-products.addEventListener('click',productShower);
+var incrementProductsShowImgs = function (event) {
+  if (clicks < 24) {
+    if (event.target.id === 'product1') {
+      var index = lastImageArray[0];
+      productArray[index].clickCounter++;
+    } else if (event.target.id === 'product2') {
+      var index = lastImageArray[1];
+      productArray[index].clickCounter++;
+    } else {
+      var index = lastImageArray[2];
+      productArray[index].clickCounter++;
+    }
+    clicks++;
+    productShower();
+  } else {
+    var products = document.getElementById('products');
+    products.style.display = 'none';
+    displayList();
+  }
+};
+
+var button = document.getElementById('start-button');
+var productImg = document.getElementsByClassName('productImg');
+
+button.addEventListener('click',startButton);
+for (var i = 0; i < productImg.length; i++) {
+  productImg[i].addEventListener('click',incrementProductsShowImgs);
+}
+
+var displayList = function () {
+  for (var i = 0; i < productArray.length; i++) {
+    var list = document.getElementById('list');
+    var listItem = document.createElement('li');
+    listItem.innerHTML = productArray[i].clickCounter + ' votes for the ' + productArray[i].name;
+    list.appendChild(listItem);
+  }
+};
