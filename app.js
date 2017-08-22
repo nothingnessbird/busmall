18fb50394c4faf286cf50abdb7569cc1f2dce93e
Kinley Ramson <kinleyramson@gmail.com>
2017-08-22 12:55:36 -0700

    changed the opacity for the colors and borders of the shown data

--- app.js
+++ app.js
@@ -108,7 +108,7 @@ var chartData = function () {
 var chartConfig = {
   type: 'horizontalBar',
   data: {
-    labels: nameArray, // x-axis labels for every entry in your data set. It should match up with the number of things you're plotting (if it's a bar chart)
+    labels: nameArray,
     datasets: [{ // <-- notice that this can be an array of multiple data sets.
       // each data set is its own object literal.
       label: '# of Votes', // <-- the label of this one data set
@@ -133,6 +133,54 @@ var chartConfig = {
         'rgba(100, 25, 255, 0.4)',
         'rgba(160, 160, 204, 0.4)',
         'rgba(25, 197, 164, 0.4)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 0.6)',
+        'rgba(54, 162, 235, 0.6)',
+        'rgba(255, 206, 86, 0.6)',
+        'rgba(75, 192, 192, 0.6)',
+        'rgba(153, 102, 255, 0.6)',
+        'rgba(20, 109, 164, 0.6)',
+        'rgba(55, 59, 254, 0.6)',
+        'rgba(205, 180, 94, 0.6)',
+        'rgba(150, 255, 64, 0.6)',
+        'rgba(205, 109, 204, 0.6)',
+        'rgba(30, 200, 164, 0.6)',
+        'rgba(228, 15, 294, 0.6)',
+        'rgba(255, 30, 64, 0.6)',
+        'rgba(255, 200, 24, 0.6)',
+        'rgba(130, 250, 114, 0.6)',
+        'rgba(90, 255, 150, 0.6)',
+        'rgba(100, 25, 255, 0.6)',
+        'rgba(160, 160, 204, 0.6)',
+        'rgba(25, 197, 164, 0.6)'
+      ],
+      borderWidth: 1 // border width in pixels
+    },
+    { // <-- notice that this can be an array of multiple data sets.
+      // each data set is its own object literal.
+      label: '# of Times Shown', // <-- the label of this one data set
+      data: shownDataArray, // <-- where your data actually goes. just the numbers
+      backgroundColor: [ // <-- this can be either one single color or a color for each item in your bar chart.
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(20, 109, 164, 0.2)',
+        'rgba(55, 59, 254, 0.2)',
+        'rgba(205, 180, 94, 0.2)',
+        'rgba(150, 255, 64, 0.2)',
+        'rgba(205, 109, 204, 0.2)',
+        'rgba(30, 200, 164, 0.2)',
+        'rgba(228, 15, 294, 0.2)',
+        'rgba(255, 30, 64, 0.2)',
+        'rgba(255, 200, 24, 0.2)',
+        'rgba(130, 250, 114, 0.2)',
+        'rgba(90, 255, 150, 0.2)',
+        'rgba(100, 25, 255, 0.2)',
+        'rgba(160, 160, 204, 0.2)',
+        'rgba(25, 197, 164, 0.2)'
       ],
       borderColor: [
         'rgba(255, 99, 132, 1)',
